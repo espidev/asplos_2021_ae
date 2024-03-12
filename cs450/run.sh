5f2a21cdc83f4ed8fcec3dd1231fe54ab98f5dfa
Devin Lin <espidev@gmail.com>
2024-03-12 17:30:23 +0000

    change run.sh

--- cs450/run.sh
+++ cs450/run.sh
@@ -1,4 +1,4 @@
 cd util/correlation
 # time ./run_hw.py -c -R 1 -D 0 -B oo-cuda,oo-concord,oo-mem,oo-coal,oo-tp
-time ./run_hw.py -c -R 1 -D 0 -B oo-coal
-cd ../../../
+time ./run_hw.py -c -R 1 -D 0 -B oo-mem
+cd ../../
